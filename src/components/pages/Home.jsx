8acd7651ce8f216d770e740580c6fc4b96aa23f5
rohantastic <rohantastic@outlook.com>
2023-10-28 20:47:44 +0530

    expenses listing

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,6 +6,33 @@ import Footer from '../common/Footer';
 
 const Home = () => {
     const [showNotification, setShowNotification] = useState(true);
+    const [expenses,setExpenses] = useState([]);
+
+    const [expenseData, setExpenseData] = useState({
+        moneySpent: "",
+        description: "",
+        category: "Food", 
+    });
+
+    const handleExpenseChange = (e) => {
+        const { name, value } = e.target;
+        setExpenseData({
+            ...expenseData,
+            [name]: value,
+        });
+    };
+
+    const handleExpenseSubmit = (e) => {
+        e.preventDefault();
+        setExpenses([...expenses,expenseData]);
+
+        setExpenseData({
+            moneySpent: "",
+            description: "",
+            category: "Food",
+        });
+        console.log("Expense data:", expenseData);
+    };
 
     useEffect(() => {
         if (showNotification) {
@@ -17,15 +44,15 @@ const Home = () => {
 
     const handleVerifyEmail = async () => {
         try {
-            const response = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCC0VjOlGiP9AhJi_tof2grQMe-B0T1-lU", {
+            const response = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=YOUR_API_KEY", {
                 method: "POST",
                 body: JSON.stringify({
                     requestType: "VERIFY_EMAIL",
-                    idToken: localStorage.getItem("idToken")
+                    idToken: localStorage.getItem("idToken"),
                 }),
                 headers: {
                     'Content-Type': 'application/json',
-                }
+                },
             });
 
             if (response.ok) {
@@ -37,7 +64,7 @@ const Home = () => {
         } catch (err) {
             console.log(err);
         }
-    }
+    };
 
     return (
         <>
@@ -53,6 +80,60 @@ const Home = () => {
             )}
 
             <button onClick={handleVerifyEmail}>Verify Email</button>
+
+            {/* Expense Entry Form */}
+            <form className="form-container" onSubmit={handleExpenseSubmit}>
+                <h2>Expense Entry</h2>
+                <label>
+                    Money Spent:
+                    <input
+                        type="number"
+                        name="moneySpent"
+                        value={expenseData.moneySpent}
+                        onChange={handleExpenseChange}
+                        required
+                    />
+                </label>
+                <br />
+                <label>
+                    Description:
+                    <input
+                        type="text"
+                        name="description"
+                        value={expenseData.description}
+                        onChange={handleExpenseChange}
+                        required
+                    />
+                </label>
+                <br />
+                <label>
+                    Category:
+                    <select
+                        name="category"
+                        value={expenseData.category}
+                        onChange={handleExpenseChange}
+                    >
+                        <option value="Food">Food</option>
+                        <option value="Petrol">Petrol</option>
+                        <option value="Salary">Salary</option>
+                        
+                    </select>
+                </label>
+                <br />
+                <button type="submit">Submit Expense</button>
+            </form>
+            
+            <div className="expense-list">
+                <h2>Expenses</h2>
+                <ul>
+                    {expenses.map((expense, index) => (
+                        <li key={index}>
+                            <strong>Category:</strong> {expense.category}, <strong>Money Spent:</strong> ${expense.moneySpent}, <strong>Description:</strong> {expense.description}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            
             <Footer />
         </>
     );
